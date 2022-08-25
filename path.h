e1a348cda566316fa2d581aefbdc06684005b12f
mug3njutsu <johngitahi272@gmail.com>
2022-08-25 23:00:22 +0300

    path.h

---
+++ path.h
@@ -0,0 +1,12 @@
+#ifndef PATH_H
+#define PATH_H
+
+#include "main.h"
+#include "info.h"
+#include "list.h"
+#include "string.h"
+#include "types.h"
+
+char *search_path(info_t *info, list_t *path);
+
+#endif /* PATH_H */
