8b4c52f24e306ae815af228c9a7f114ff1a7fb9f
mug3njutsu <johngitahi272@gmail.com>
2022-08-25 20:45:16 +0300

    isalpha.c

---
+++ isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * _isalpha - checks if alphabetic
+  * @c: char
+  * Return: 0 || 1
+  */
+
+bool isalpha(int c)
+{
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
