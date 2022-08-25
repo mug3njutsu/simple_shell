7f70c92cd41da631e8d04aade48f78ddb8e7ed3a
mug3njutsu <johngitahi272@gmail.com>
2022-08-25 21:27:50 +0300

    cmdtree.c

---
+++ cmdtree.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cmd_to_tree - construct a binary tree of commands
+ * @tokens: command to parse
+ * Return: NULL || &new_root
+ */
+
+cmdtree_t *cmd_to_tree(const char * const *tokens __attribute__((unused)))
+{
+	return (NULL);
+}
+
+/**
+ * free_cmdtree - free a binary tree and set root to NULL
+ * @rootptr: *p
+ * Return: NULL
+ */
+
+void free_cmdtree(cmdtree_t **rootptr)
+{
+	if (rootptr && *rootptr)
+	{
+		free_cmdtree(&((*rootptr)->success));
+		free_cmdtree(&((*rootptr)->failure));
+		(*rootptr)->tokens = NULL;
+		(*rootptr) = NULL;
+	}
+}
