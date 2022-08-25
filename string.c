d66597283a196eb2c6bfe3e4044c6e88b5ef52bb
mug3njutsu <johngitahi272@gmail.com>
2022-08-25 16:24:34 +0300

    string.c

---
+++ string.c
@@ -0,0 +1,115 @@
+#include "shell.h"
+
+/**
+ * _strdup - returns *p -> newly allocated space in memory
+ * @str: *p -> str
+ * Return: *p -> str
+ */
+
+char *_strdup(char *str)
+{
+	int i, l;
+	char *new;
+
+	if (!str)
+	{
+		return (NULL);
+	}
+	for (l = 0; str[l] != '\0';)
+	{
+		l++;
+	}
+	new = malloc(sizeof(char) * l + 1);
+	if (!new)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < l; i++)
+	{
+		new[i] = str[i];
+	}
+	new[l] = str[l];
+	return (new);
+}
+
+/**
+ * concat_all - concats 3 strs in a newly allocated memory
+ * @name: first str
+ * @sep: second str
+ * @value: third str
+ * Return: *p -> new str
+ */
+
+char *concat_all(char *name, char *sep, char *value)
+{
+	char *result;
+	int l1, l2, l3, i, k;
+
+	l1 = _strlen(name);
+	l2 = _strlen(sep);
+	l3 = _strlen(value);
+
+	result = malloc(l1 + l2 + l3 + 1);
+	if (!result)
+		return (NULL);
+
+	for (i = 0; name[i]; i++)
+		result[i] = name[i];
+	k = i;
+
+	for (i = 0; sep[i]; i++)
+		result[k + i] = sep[i];
+	k = k + i;
+
+	for (i = 0; value[i]; i++)
+		result[k + i] = value[i];
+	k = k + i;
+
+	result[k] = '\0';
+
+	return (result);
+}
+
+/**
+ * _strlen - len(str)
+ * @s: *p -> str
+ * Return: len(str)
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _putchar - writes char to stdout
+ * @c: char to print
+ * Return: on success 1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _puts - prints str
+ * @str: *p -> str
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
