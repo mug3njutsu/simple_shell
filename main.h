a865c5b90cb3eeae0181d3607d22caf235ea4d85
mug3njutsu <johngitahi272@gmail.com>
2022-08-25 22:29:09 +0300

    main.h

--- main.h
+++ main.h
@@ -321,11 +321,11 @@ quote_state_t quote_state(char c);
 quote_state_fp quote_state_f(quote_state_t s);
 
 size_t quote_state_len(const char *str, quote_state_t state);
-size_t quote_state_none(const char *str, quote_state_t *state);
-size_t quote_state_word(const char *str, quote_state_t *state);
-size_t quote_state_double(const char *str, quote_state_t *state);
-size_t quote_state_single(const char *str, quote_state_t *state);
-size_t quote_state_escape(const char *str, quote_state_t *state);
+size_t _quote_state_none(const char *str, quote_state_t *state);
+size_t _quote_state_word(const char *str, quote_state_t *state);
+size_t _quote_state_double(const char *str, quote_state_t *state);
+size_t _quote_state_single(const char *str, quote_state_t *state);
+size_t _quote_state_escape(const char *str, quote_state_t *state);
 
 int isspecial_double(char c);
 
