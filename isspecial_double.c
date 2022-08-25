96b9d3ca103f0cdeb65bb972e601eb1a625e3f17
mug3njutsu <johngitahi272@gmail.com>
2022-08-25 21:38:36 +0300

    isspecial_double.c

--- isspecial_double.c
+++ isspecial_double.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isspecial_double - check if special char in double quotes
+ * isspecial_double - check if special char in double quotes
  * @c: char to check
  * Return: 0 || 1
  */
