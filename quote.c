2b6f7e7a3430611d28e3059341536cdcde6a3cb2
mug3njutsu <johngitahi272@gmail.com>
2022-08-25 22:32:03 +0300

    quote.c

---
+++ quote.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * quote_state - get the state associated with a given char
+ * @c: char
+ * Return: the state associated with c
+ */
+
+quote_state_t quote_state(char c)
+{
+	if (isspacechar(c))
+		return (QUOTE_NONE);
+	if (c == '"')
+		return (QUOTE_DOUBLE);
+	if (c == '\'')
+		return (QUOTE_SINGLE);
+	if (c == '\\')
+		return (QUOTE_ESCAPE);
+	return (QUOTE_WORD);
+}
+
+/**
+ * quote_state_f - get the function associated with a given state
+ * @s: state
+ * Return: the state associated with c
+ */
+
+quote_state_fp quote_state_f(quote_state_t s)
+{
+	switch (s)
+	{
+	case QUOTE_NONE:
+		return (_quote_state_none);
+	case QUOTE_WORD:
+		return (_quote_state_word);
+	case QUOTE_DOUBLE:
+		return (_quote_state_double);
+	case QUOTE_SINGLE:
+		return (_quote_state_single);
+	case QUOTE_ESCAPE:
+		return (_quote_state_escape);
+	}
+	return (NULL);
+}
+
+/**
+ * quote_state_len - get the len(given state)
+ * @str: str
+ * @state: state
+ * Return: the state associated with c
+ */
+
+size_t quote_state_len(const char *str, quote_state_t state)
+{
+	return (quote_state_f(state)(str, NULL));
+}
