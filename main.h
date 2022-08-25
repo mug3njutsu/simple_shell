7b4b1ca80add2848fc468d63248b939f26ebe80f
mug3njutsu <johngitahi272@gmail.com>
2022-08-25 23:22:36 +0300

    main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#endif
