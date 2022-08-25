d31bcfc3580d431b15c2b3ec741a90aaec992693
mug3njutsu <johngitahi272@gmail.com>
2022-08-25 20:50:36 +0300

    changedir.c

--- changedir.c
+++ changedir.c
@@ -11,9 +11,9 @@ void __cd_error(info_t *info, char *dir)
 	char *error = strjoin(NULL, " ", "can't cd to", dir);
 
 	perrorl_default(*info->argv, info->lineno, error, *info->tokens, NULL);
-	
+
 	info->status = 2;
-	
+
 	free(error);
 }
 
