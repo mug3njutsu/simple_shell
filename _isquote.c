f1c254220d026a1702168e1c8e1305d597cd0d44
mug3njutsu <johngitahi272@gmail.com>
2022-08-26 12:43:35 +0300

    _isquote.c

--- _isquote.c
+++ _isquote.c
@@ -1,10 +1,11 @@
 #include "ctype.h"
 
 /**
- * _isquote - check if a character is a quote
- * @c: the character to check
- * Return: If c is a quote, return 1. Otherwise, return 0.
+ * _isquote - check if char is a quote
+ * @c: char to check
+ * Return: 0 || 1
  */
+
 bool _isquote(int c)
 {
 	return (c == '"' || c == '\'' || c == '\\');
