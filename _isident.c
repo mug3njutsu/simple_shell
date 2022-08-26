2804836dfa225649d076aa82bc4f23a42aafc522
mug3njutsu <johngitahi272@gmail.com>
2022-08-26 12:43:29 +0300

    _isident.c

--- _isident.c
+++ _isident.c
@@ -1,10 +1,11 @@
 #include "ctype.h"
 
 /**
-  * _isident - checks if the character is a valid identifier character
-  * @c: character to check
- * Return: If c is an identifier character, return 1. Otherwise, return 0.
+  * _isident - checks if the char is a valid identifier char
+  * @c: char to check
+  * Return: 0 || 1
   */
+
 bool _isident(int c)
 {
 	return (c == '_' || _isalnum(c));
