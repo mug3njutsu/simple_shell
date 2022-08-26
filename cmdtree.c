a56801abb60624521d8f863d4ffd88af7ed3112c
mug3njutsu <johngitahi272@gmail.com>
2022-08-26 12:42:40 +0300

    cmdtree.c

--- cmdtree.c
+++ cmdtree.c
@@ -2,21 +2,21 @@
 
 /**
  * cmd_to_tree - construct a binary tree of commands
- * @tokens: the command to parse
- *
- * Return: If memory allocation fails, return NULL. Otherwise, return the
- * address of the root of the new tree
+ * @tokens: command to parse
+ * Return: NULL || &root of the new tree
  */
+
 cmdtree_t *cmd_to_tree(const char * const *tokens __attribute__((unused)))
 {
 	return (NULL);
 }
 
 /**
- * free_cmdtree - free a binary tree and and set root to NULL
- * @rootptr: pointer
+ * free_cmdtree - free a binary tree and set root to NULL
+ * @rootptr: *p
  * Return: NULL
  */
+
 void free_cmdtree(cmdtree_t **rootptr)
 {
 	if (rootptr && *rootptr)
