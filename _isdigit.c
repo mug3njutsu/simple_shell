9979b4bbb38dd49fcf3de69c3fd5aa5756437c56
mug3njutsu <johngitahi272@gmail.com>
2022-08-26 12:43:25 +0300

    _isdigit.c

--- _isdigit.c
+++ _isdigit.c
@@ -1,10 +1,11 @@
 #include "ctype.h"
 
 /**
-  * _isdigit - checks if the character is digit
-  * @c: character to check
+  * _isdigit - checks if the char is digit
+  * @c: char to check
   * Return: int
   */
+
 bool _isdigit(int c)
 {
 	return (c >= '0' && c <= '9');
