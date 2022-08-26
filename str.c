29d02e40b0aea8e9552ac7fa6c6d1622a5f0233c
mug3njutsu <johngitahi272@gmail.com>
2022-08-26 12:44:06 +0300

    str.c

--- str.c
+++ str.c
@@ -1,11 +1,12 @@
 #include "string.h"
 
 /**
-  * _strchr - get the index of the first matching character
-  * @str: string passed
-  * @chr: character passed
+  * _strchr - get the index of the first matching char
+  * @str: str passed
+  * @chr: char passed
   * Return: index
   */
+
 ssize_t _strchr(const char *str, char chr)
 {
 	ssize_t index;
@@ -22,13 +23,12 @@ ssize_t _strchr(const char *str, char chr)
 	return (-1);
 }
 
-
 /**
- * _strdup - create a new array containing a copy of the given string
- * @str: a pointer to the string to copy
- * Return: If str is NULL or if memory allocation fails, return NULL.
- * Otherwise a return a pointer to the new copy
+ * _strdup - create a new []] containing a copy of the given str
+ * @str: *p -> str to copy
+ * Return: NULL || *p -> new copy
  */
+
 char *_strdup(const char *str)
 {
 	char *dup;
@@ -50,12 +50,12 @@ char *_strdup(const char *str)
 	return (dup);
 }
 
-
 /**
- * _strlen - calculate the length of a string
- * @str: the string to calculate the length of
- * Return: the length of the string
+ * _strlen - calculate the length of a str
+ * @str: str to calculate the length
+ * Return: len(str)
  */
+
 ssize_t _strlen(const char *str)
 {
 	const char *pos = str;
@@ -69,15 +69,13 @@ ssize_t _strlen(const char *str)
 	return (pos - str);
 }
 
-
 /**
  * _strcmp - compare two strings
- * @s1: a string to compare
- * @s2: the other string to compare
- * Return: 0 if s1 matches s2,
- * otherwise an integer less than 0 if s1 is less than s2,
- * otherwise an integer greater than 0 if s1 is greater than s2.
+ * @s1: a str to compare
+ * @s2: the other str to compare
+ * Return: << 0 >>
  */
+
 int _strcmp(const char *s1, const char *s2)
 {
 	for (; *s1 && *s2; ++s1, ++s2)
@@ -94,13 +92,13 @@ int _strcmp(const char *s1, const char *s2)
 	return (0);
 }
 
-
 /**
- * _strcpy - copy a string
- * @dest: destination
- * @src: source
- * Return: a pointer to dest
+ * _strcpy - copy str
+ * @dest: dest
+ * @src: src
+ * Return: *p -> dest
  */
+
 char *_strcpy(char *dest, const char *src)
 {
 	char *pos = dest;
